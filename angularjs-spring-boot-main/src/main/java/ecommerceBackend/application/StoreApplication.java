07de46ee4b39eef3d1e47b70f285a8431c5bba2d
Alvin Lau <bobthebuilder500000@gmail.com>
2022-12-14 03:10:22 -0500

    fixed the cors thing

--- angularjs-spring-boot-main/src/main/java/ecommerceBackend/application/StoreApplication.java
+++ angularjs-spring-boot-main/src/main/java/ecommerceBackend/application/StoreApplication.java
@@ -3,8 +3,11 @@ package ecommerceBackend.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "ecommerceBackend")
@@ -19,4 +22,13 @@ public class StoreApplication {
       SpringApplication.run(StoreApplication.class, args);
   }
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
--- angularjs-spring-boot-main/src/main/java/ecommerceBackend/controller/UserController.java
+++ angularjs-spring-boot-main/src/main/java/ecommerceBackend/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
